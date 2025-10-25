b4467109b4a301c3240d3b01670774a5f17b7ea8
Ryan Smith <rysmith2113@gmail.com>
2025-10-25 15:37:13 -0400

    Change the body font

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,18 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import { Fira_Code as FontMono, Rubik as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+	subsets: [
+		"arabic",
+		"cyrillic",
+		"cyrillic-ext",
+		"hebrew",
+		"latin",
+		"latin-ext"
+	],
+	variable: "--font-sans",
 });
 
 export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+	subsets: ["latin"],
+	variable: "--font-mono",
 });
