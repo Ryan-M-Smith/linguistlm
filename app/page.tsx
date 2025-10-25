ccd1620aa25362f52bacab9ca595e3bd3d4ba4cf
Ryan Smith <rysmith2113@gmail.com>
2025-10-25 15:58:34 -0400

    Change body text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { JSX } from "react";
 export default function Home(): JSX.Element {
 	return (
 		<main className="flex justify-center items-center">
-			<h1 className="text-4xl text-center">
+			<h1 className="text-4xl text-center text-llm-lace">
 				LinguistLM
 			</h1>
 		</main>
