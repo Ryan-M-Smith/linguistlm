faf9f6219c724c1c9a57980bf675b3dc8d71f64c
Ryan Smith <rysmith2113@gmail.com>
2025-10-25 18:49:59 -0400

    Update UI on the writing page

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -15,19 +15,43 @@ export default function Write(): JSX.Element {
 	);
 	
 	return (
-		<div className="flex justify-center h-full w-full overflow-hidden">
+		<div className="flex justify-center w-full h-full overflow-hidden">
 			<div className="flex gap-x-4 w-[90%] p-4">
 				<textarea
-					className="flex-1 h-full p-4 bg-default-50 text-foreground resize-none rounded-xl"
+					className={`
+						flex-1 h-full p-4 border-1 dark:bg-default-50 dark:border-llm-sea-glass
+						bg-llm-blue-flower/10 border-llm-masala resize-none rounded-xl
+						outline-none text
+					`}
 					placeholder="Enter or paste your text..."
 					style={{ fontSize: "1rem" }}
 				/>
 
-				<div className="bg-default-50 w-[30%] rounded-xl flex flex-col justify-end">
+				<div
+					className={`
+						border-1 dark:bg-default-50 dark:border-llm-sea-glass bg-llm-blue-flower/10
+						border-llm-masala w-[30%] rounded-xl flex flex-col justify-end
+						hover:bg-llm-blue-flower/10 hover:dark:bg-default-50
+						hover:border-llm-masala transition-none
+					`}
+				>
 					<Textarea
 						className="p-4 overflow-y-auto resize-none"
+						classNames={{
+							inputWrapper: `border-1 dark:bg-default-50 dark:border-llm-sea-glass
+										   dark:bg-llm-masala bg-llm-lace hover:bg-llm-lace
+										   hover:dark:bg-llm-masala transition-none`
+						}}
 						radius="lg"
-						endContent={<SendButton/>}
+						endContent={
+							<Button
+								className="transition-none hover:bg-transparent hover:opacity-100"
+								variant="ghost"
+								radius="full"
+								startContent={<FaCircleArrowUp size={30} />}
+								isIconOnly
+							/>
+						}
 						maxRows={4}
 					/>
 				</div>
